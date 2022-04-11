805de84a85bdd5335e4a0d4fde968782fa554262
Mara Rocha Fernández <maranhadisenha@gmail.com>
2022-04-11 22:02:43 +0200

    refactor api calls

--- 04-spotiapp/src/app/services/spotify.service.ts
+++ 04-spotiapp/src/app/services/spotify.service.ts
@@ -8,12 +8,18 @@ import { map } from 'rxjs/operators';
 export class SpotifyService {
   constructor(private http: HttpClient) {}
 
-  getNewReleases() {
+  getQuery(query: string) {
+    const url = `https://api.spotify.com/v1/${query}`;
+
     const headers = new HttpHeaders({
       Authorization:
-        'Bearer BQDUJAaDoJUuYyodTOi0V7qzlwqcrTb-iAbH9GMdLsLQ07H-W9xeXsN0AqMw-jZ6HD0GER5dce_H7Hs8Bks',
+        'Bearer BQDA0-p6UMC0FYH8kwW7yYBhW6WwI3MQLIlD_lpGAjjbZIZpku1EzKGsEc1w6wD7dtuyxs2Sjol5LUPW1pM',
     });
 
+    return this.http.get(url, { headers });
+  }
+
+  getNewReleases() {
     /*
     this.http
       .get('https://api.spotify.com/v1/browse/new-releases', { headers }) //?limit=5
@@ -22,23 +28,14 @@ export class SpotifyService {
       });
     */
 
-    return this.http
-      .get('https://api.spotify.com/v1/browse/new-releases', {
-        headers,
-      })
-      .pipe(map((data: any) => data['albums'].items));
+    return this.getQuery('browse/new-releases?limit=20').pipe(
+      map((data: any) => data['albums'].items)
+    );
   }
 
   getArtist(term: string) {
-    const headers = new HttpHeaders({
-      Authorization:
-        'Bearer BQDUJAaDoJUuYyodTOi0V7qzlwqcrTb-iAbH9GMdLsLQ07H-W9xeXsN0AqMw-jZ6HD0GER5dce_H7Hs8Bks',
-    });
-
-    return this.http
-      .get(`https://api.spotify.com/v1/search?q=${term}&type=artist&limit=15`, {
-        headers,
-      })
-      .pipe(map((data: any) => data['artists'].items));
+    return this.getQuery(`search?q=${term}&type=artist&limit=15`).pipe(
+      map((data: any) => data['artists'].items)
+    );
   }
 }
